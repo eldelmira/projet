142f92de99e5519dc875f01a9e40c25444a54878
eldelmrira <eldelmrirasmart@outlook.fr>
2024-10-07 16:53:06 -0300

    v2

--- TP_JS1/test.js
+++ TP_JS1/test.js
@@ -25,3 +25,57 @@ function affiche(){
     document.write("Bonjour  "+ prenom+ "   "+nom+ ".  "+"Vous avez"+ "   "+age+"  "+ "ans");
     document.write("</div>");
 }
+
+function test_couleur(){
+    let couleur = prompt("Donner une couleur")
+    if (couleur == "red"){ 
+        document.body.style.backgroundColor = "red"
+    }
+
+    else if (couleur == "lightblue"){ 
+        document.body.style.backgroundColor = "lightblue"
+    } 
+
+    else if (couleur == "lightgreen"){ 
+        document.body.style.backgroundColor = "lightgreen"
+
+    } 
+
+    else if (couleur == "lightpink"){ 
+        document.body.style.backgroundColor = "lightpink"
+    } 
+
+    else if (couleur == "teal"){ 
+        document.body.style.backgroundColor = "teal"
+    } 
+    else
+    {
+        document.write("Couleur non comprise")
+    }
+}
+
+function calcul_moyenne(){
+    var n1 = prompt("Donner la première note : ");
+    var n2 = prompt("Donner la deuxième note : ");
+    var n3 = prompt("Donner la troisième note : ");
+
+    var somme = Number(n1)+Number(n2)+Number(n3);
+
+    document.write("Voici la somme : "+ somme+ "<br>")
+    var moyenne = somme/3;
+
+    document.write("Voici la moyenne :" + moyenne+"<br>")
+
+    if (moyenne < 10){
+        document.write("Vous n'êtes pas ADMIS ")
+    }
+
+    else if (moyenne <14){
+        document.write("Vous êtes ADMIS passable ")
+    }
+
+    else
+    {
+        document.write("Vous êtes ADMIS bien ")
+    }
+}
